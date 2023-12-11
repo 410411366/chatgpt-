4eb1f71b142505cbedca8aea9bd0b3165e738d86
410411366 <410411366@gms.tku.edu.tw>
2023-12-11 23:02:06 +0800

    12.05 first commit

--- Extension samples/專題/script.js
+++ Extension samples/專題/script.js
@@ -1,3 +1,21 @@
-document.getElementById('clickMe').addEventListener('click', function() {
-    alert('Button clicked!');
-  });
+function sendMessage() {
+  const userInput = document.getElementById('userInput').value;
+  displayUserMessage(userInput);
+  
+  // 向 ChatGPT API 發送用戶輸入的訊息，並獲取回應
+  // 這裡需要使用ChatGPT的API，具體代碼取決於API的用法
+
+  // 假設收到ChatGPT回應後的處理
+  const chatGPTResponse = "ChatGPT's response."; // 假設回應是這個
+  displayChatGPTMessage(chatGPTResponse);
+}
+
+function displayUserMessage(message) {
+  const chatLog = document.getElementById('chatLog');
+  chatLog.innerHTML += <div class="message user-message">${message}</div>;
+}
+
+function displayChatGPTMessage(message) {
+  const chatLog = document.getElementById('chatLog');
+  chatLog.innerHTML += <div class="message chatGPT-message">${message}</div>;
+}
