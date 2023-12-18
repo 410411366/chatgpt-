d4c0d8cb7e7a1e72e01ba191f59beb56a0153de0
410411366 <410411366@gms.tku.edu.tw>
2023-12-18 21:46:37 +0800

    12.12 first commit

--- Extension samples/專題/script.js
+++ Extension samples/專題/script.js
@@ -1,21 +1,21 @@
-function sendMessage() {
-  const userInput = document.getElementById('userInput').value;
-  displayUserMessage(userInput);
-  
-  // 向 ChatGPT API 發送用戶輸入的訊息，並獲取回應
-  // 這裡需要使用ChatGPT的API，具體代碼取決於API的用法
 
-  // 假設收到ChatGPT回應後的處理
-  const chatGPTResponse = "ChatGPT's response."; // 假設回應是這個
-  displayChatGPTMessage(chatGPTResponse);
-}
 
-function displayUserMessage(message) {
-  const chatLog = document.getElementById('chatLog');
-  chatLog.innerHTML += <div class="message user-message">${message}</div>;
-}
+// 創建一個新的按鈕
+let translateButton = document.createElement('button');
+translateButton.textContent = '翻譯';
+translateButton.id = 'translateButton';
 
-function displayChatGPTMessage(message) {
-  const chatLog = document.getElementById('chatLog');
-  chatLog.innerHTML += <div class="message chatGPT-message">${message}</div>;
-}
+// 定義按鈕的點擊事件
+translateButton.onclick = function() {
+  // 獲取用戶的輸入
+  let userText = document.getElementById('userInput').value;
+
+  // 創建一個特定的prompt
+  let prompt = '將以下的文字翻譯成英文：' + userText;
+
+  // 將prompt插入到輸入框中
+  document.getElementById('userInput').value = prompt;
+};
+
+// 將按鈕添加到頁面上
+document.body.appendChild(translateButton);
